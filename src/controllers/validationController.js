22a0ca3eb89116d03c48b57fcfa39aa132c80a09
medu <m.mustafa@linguala.com>
2018-11-01 17:47:04 +0100

    added validation controller

---
+++ src/controllers/validationController.js
@@ -0,0 +1,19 @@
+let ValidationController = {
+    "EMAIL_VALIDATOR": (email) => {
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        return reg.test(email) === true
+    },
+    "MOBILE_NUMBER_VALIDATOR": (mobile_number) => {
+        let company_code = mobile_number.substring(0, 3)
+        let company_Code_Ok = company_code === "079" || company_code === "078" || company_code === "078" ? true : false
+        if (company_Code_Ok === true) {
+            return mobile_number.length === 10
+        }
+        return false
+    },
+    "PASSWORD_VALIDATOR": (pwd, confirm_pwd) => {
+        return pwd === confirm_pwd
+    }
+}
+
+module.exports = ValidationController
