47cd8e630e3af23eb0d752522ec44eaf4537b499
moddather rashed <moddather.developer@gmail.com>
2019-01-16 22:45:45 +0100

    make some changes on the checkout screen design

--- src/screens/CartScreens/CheckoutScreen.js
+++ src/screens/CartScreens/CheckoutScreen.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
-import { View, Text, Dimensions, TextInput, I18nManager, ScrollView } from 'react-native'
+import { View, Text, Dimensions, TextInput, I18nManager, ScrollView, AsyncStorage, TouchableOpacity, Image, FlatList } from 'react-native'
 import { Button } from 'native-base'
 import translation from './../../controllers/translation'
+import ApiController from './../../controllers/ApiController'
+
 const viewportWidth = Dimensions.get('window').width
 
 class CheckoutScreen extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            locations: null
         }
+    }
+    getAddresses() {
+        AsyncStorage.getItem('user_id').then((item) => {
+            ApiController.get_user_addresses(item)
+                .then((res) => {
+                    if (res.status === 1) {
+                        this.setState({
+                            locations: res.addresses
+                        })
+                    }
+                })
+        })
     }
     scalling(size) {
         return viewportWidth * (size / viewportWidth)
+    }
+    componentDidMount() {
+        this.getAddresses()
     }
     static navigationOptions = () => ({
         title: 'Last Step',
@@ -81,24 +98,56 @@ class CheckoutScreen extends Component {
                     numberOfLines={10}
                     onChangeText={(user_report) => this.setState({ user_report })}
                     value={this.state.user_report} />
-                <Button rounded
-                    onPress={() => {
-                        this.props.navigation.navigate('CheckoutScreen')
-                    }}
-                    style={{
-                        backgroundColor: '#638bba',
-                        borderRadius: 25,
-                        borderColor: 'white',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        alignSelf: 'center',
-                        width: 200,
-                        marginTop: 20,
-                        marginBottom: 10,
-                        borderWidth: 1
-                    }} >
-                    <Text style={{ fontWeight: '700', color: 'white' }}>Confirm</Text>
-                </Button>
+                <View style={{ flex: 1 }}>
+                    <View style={{ flex: 1, justifyContent: 'center' }}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                this.setState({
+                                    checked: 'ar'
+                                })
+                            }}
+                            style={{ flexDirection: 'row', flex: 1, alignItems: 'center' }}>
+                            <Image source={this.state.checked === 'en' ? require('../../Icons/Uncheck.png') : require('../../Icons/Check.png')} resizeMode='contain' style={{ flex: 0.2, height: 24 }} />
+                            <Text style={{ fontSize: 17, flex: 0.8, textAlign: 'left' }}>Add New</Text>
+                        </TouchableOpacity>
+                        <FlatList
+                            horizontal={false}
+                            keyExtractor={item => item.id}
+                            contentContainerStyle={{ flex: 1, }}
+                            data={this.state.locations}
+                            renderItem={({ item, index }) =>
+                                <TouchableOpacity
+                                    onPress={() => {
+                                        this.setState({
+                                            checked: 'ar'
+                                        })
+                                    }}
+                                    style={{ flexDirection: 'row', flex: 1, alignItems: 'center' }}>
+                                    <Image source={this.state.checked === 'en' ? require('../../Icons/Uncheck.png') : require('../../Icons/Check.png')} resizeMode='contain' style={{ flex: 0.2, height: 24 }} />
+                                    <Text style={{ fontSize: 17, flex: 0.8, textAlign: 'left' }}>{item.address}</Text>
+                                </TouchableOpacity>
+                            }
+                        />
+                    </View>
+                    <Button rounded
+                        onPress={() => {
+                            this.props.navigation.navigate('CheckoutScreen')
+                        }}
+                        style={{
+                            backgroundColor: '#638bba',
+                            borderRadius: 25,
+                            borderColor: 'white',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            alignSelf: 'center',
+                            width: 200,
+                            marginTop: 20,
+                            marginBottom: 10,
+                            borderWidth: 1
+                        }} >
+                        <Text style={{ fontWeight: '700', color: 'white' }}>Confirm</Text>
+                    </Button>
+                </View>
             </ScrollView>
         )
     }
